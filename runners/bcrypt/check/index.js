904bd4792049772908b72dff8bcc89802206b221
novokhatskyioleksii <avecezar17@gmail.com>
2020-05-14 09:05:32 +0300

    bcrypt check fix

--- runners/bcrypt/check/index.js
+++ runners/bcrypt/check/index.js
@@ -6,7 +6,7 @@ const check = () => {
   const result = [];
 
   const beforeAll = microseconds.now();
-  for (let i = 0; i < 1400000; i++) {
+  for (let i = 0; i < 140; i++) {
     const before = microseconds.now();
     const hash = file[i];
     const after = before + microseconds.since(before);
